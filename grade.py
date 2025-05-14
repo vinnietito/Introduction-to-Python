118bb78a5941a9217d8bec572a616f6231eee8cb
vinnietito <vincentnjuguna69@gmail.com>
2025-05-14 11:31:07 +0300

    Initial commits

---
+++ grade.py
@@ -0,0 +1,5 @@
+student = “Philip Oyier"
+grades = ["A","B","C","D","D","C"]
+gradeDict = {}
+gradeDict[student] = grades
+print( gradeDict[“Philip Oyier"] )
---
+++ polynomialFunction.py
@@ -0,0 +1,6 @@
+def polynomial(x):
+    return 3 * (x ** 2) - x + 2
+
+#Test
+result = polynomial(3)
+print("Results:", result)
