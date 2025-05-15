f6881eccab116620e1c0a0fa22124861f1493ef6
vinnietito <vincentnjuguna69@gmail.com>
2025-05-15 12:03:21 +0300

    Commits

--- classExtendingStudent.py
+++ classExtendingStudent.py
@@ -11,7 +11,12 @@ class Student:
 # Derived class
 class BIT_student(Student):
     """A class extending Student."""
-    def __init__(self, n, a):
+    def __init__(self, n, a, d):
+        #Call __init__ of the parent class
         super().__init__(n, a)
         self.department_num = d
+        
+    def get_age(self):
+        # Overrides get_age method of base class
+        print("Age:", self.age)
         
