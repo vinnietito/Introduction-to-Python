be73e932d9db044788d0eff07a4fd3fc2030440e
vinnietito <vincentnjuguna69@gmail.com>
2025-05-15 11:51:48 +0300

    Commits

---
+++ classExtendingStudent.py
@@ -0,0 +1,11 @@
+class Student:
+    """A class representing a student"""
+    def __init__(self, n, a):
+        self.full_name = n
+        self.age = a
+        
+    def get_age(self):
+        return self.age
+    
+    
+# Derived class
