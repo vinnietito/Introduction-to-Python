6b44985427790c485ee066b6781efc7e2bf02cb7
vinnietito <vincentnjuguna69@gmail.com>
2025-05-15 11:07:54 +0300

    Commits

---
+++ CircleClassConstructor.py
@@ -0,0 +1,19 @@
+import math
+
+class Circle:
+    def __init__(self, r):
+        self.radius = r
+        
+    def calArea(self):
+        return math.pi * pow(self.radius, 2)
+    
+    
+# Main program
+aCircle = Circle(7)
+bCircle = Circle(14)
+
+areaA = aCircle.calArea()
+areaB = bCircle.calArea()
+
+print("Areaof the circle A is:\t", areaB)
+print("Area of circle B is:\t", areaB)
